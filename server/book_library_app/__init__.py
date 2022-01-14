4eec1ac97108d7a381eaf630fc2b7a06377a7a1b
Jakub <koszowskimtb@gmail.com>
2022-01-14 14:42:59 +0100

    created fun to handle http calls

--- server/book_library_app/__init__.py
+++ server/book_library_app/__init__.py
@@ -6,6 +6,4 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
-def index():
-    return 'Hello from Flask!'
+from book_library_app import authors # import ponizej tworzenia app - wazne
