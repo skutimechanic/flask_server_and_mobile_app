fefaf3d835d271698d4e5c52f6270d3f3d13f572
Jakub <koszowskimtb@gmail.com>
2022-01-14 09:44:41 +0100

    get and post handle by the same path

---
+++ server/sample_posts_api_app.py
@@ -0,0 +1,52 @@
+from urllib import response
+from flask import Flask, jsonify, request
+
+
+POSTS = [
+    {
+        'id': 1,
+        'title': 'title 1',
+        'text': 'text 1'
+    },
+    {
+        'id': 2,
+        'title': 'title 2',
+        'text': 'text 2'
+    },
+    {
+        'id': 3,
+        'title': 'title 3',
+        'text': 'text 3'
+    },
+    {
+        'id': 4,
+        'title': 'title 4',
+        'text': 'text 4'
+    },
+]
+
+
+app = Flask(__name__)
+
+
+@app.route('/posts', methods=['GET', 'POST']) # domyslna metoda to GET
+def items():
+    response_data = {
+        'success': True,
+        'data': []
+    }
+
+    if request.method == 'GET':
+        response_data['data'] = POSTS
+        return jsonify(response_data)
+    elif request.method == 'POST':
+        data = request.json
+        POSTS.append(data)
+        response_data['data'] = POSTS
+        response = jsonify(response_data)
+        response.status_code = 201
+        return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
