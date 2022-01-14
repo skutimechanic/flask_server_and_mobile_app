1035dd70744f216a766235b3bc122cf3078d2da1
Jakub <koszowskimtb@gmail.com>
2022-01-14 19:24:02 +0100

    handling delete method

--- server/book_library_app/authors.py
+++ server/book_library_app/authors.py
@@ -70,6 +70,11 @@ def update_author(args: dict, author_id: int): # wazna kolejnosc, najpierw dane 
 
 @app.route('/api/v1/authors/<int:author_id>', methods=['DELETE'])
 def delete_author(author_id: int):
+    author = Author.query.get_or_404(author_id, description=f'Author with id {author_id} not found')
+
+    db.session.delete(author)
+    db.session.commit()
+    
     return jsonify(
         {
             'success': True,
