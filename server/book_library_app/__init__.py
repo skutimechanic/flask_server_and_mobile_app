6ac42ff470010a0c045210489b15915c6146c09c
Jakub <koszowskimtb@gmail.com>
2022-01-14 16:49:58 +0100

    added migration repo and created migrate file for authors

--- server/book_library_app/__init__.py
+++ server/book_library_app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db) # dzieki temu mamy automatyczna migracje 
 
 
 from book_library_app import authors # import ponizej tworzenia app - wazne
