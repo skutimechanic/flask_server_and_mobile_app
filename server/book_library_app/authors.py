afd4381f19bcd0931fe99aeaf8cc50a158bacbc7
Jakub <koszowskimtb@gmail.com>
2022-01-14 19:22:40 +0100

    handling put method

--- server/book_library_app/authors.py
+++ server/book_library_app/authors.py
@@ -49,11 +49,21 @@ def create_author(args: dict):
 
 
 @app.route('/api/v1/authors/<int:author_id>', methods=['PUT'])
-def update_author(author_id: int):
+@validate_json_content_type
+@use_args(author_schema, error_status_code=400)
+def update_author(args: dict, author_id: int): # wazna kolejnosc, najpierw dane z walidatora pozniej dopiero id
+    author = Author.query.get_or_404(author_id, description=f'Author with id {author_id} not found')
+
+    author.first_name = args['first_name']
+    author.last_name = args['last_name']
+    author.birth_date = args['birth_date']
+
+    db.session.commit()
+
     return jsonify(
         {
             'success': True,
-            'data': f'Author with id: {author_id} has been updated'
+            'data': author_schema.dump(author)
         }
     )
 
