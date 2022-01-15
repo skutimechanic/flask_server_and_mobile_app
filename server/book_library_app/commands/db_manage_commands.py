ec752dcc26ec602d4c7bb31c7cb630040566c6be
Jakub <koszowskimtb@gmail.com>
2022-01-15 18:15:49 +0100

    books samples added

--- server/book_library_app/commands/db_manage_commands.py
+++ server/book_library_app/commands/db_manage_commands.py
@@ -2,8 +2,15 @@ import json
 from pathlib import Path
 from datetime import datetime
 from book_library_app import db
-from book_library_app.models import Author
+from book_library_app.models import Author, Book
 from book_library_app.commands import db_manage_bp
+
+
+def load_json_data(file_name: str) -> list:
+    json_path = Path(__file__).parent.parent / 'samples' / file_name
+    with open(json_path) as file:
+        data_json = json.load(file)
+    return data_json
 
 
 @db_manage_bp.cli.group()
@@ -16,13 +23,16 @@ def db_manage():
 def add_data():
     """Add sample data to database"""
     try:
-        authors_path = Path(__file__).parent.parent / 'samples' / 'authors.json'
-        with open(authors_path) as file:
-            data_json = json.load(file)
+        data_json = load_json_data('authors.json')
         for item in data_json:
             item['birth_date'] = datetime.strptime(item['birth_date'], '%d-%m-%Y').date()
             author = Author(**item) # ** rozpakowanie słownika
             db.session.add(author)
+
+        data_json = load_json_data('books.json')
+        for item in data_json:
+            book = Book(**item) # ** rozpakowanie słownika
+            db.session.add(book)
         db.session.commit()
         print('Data has been successfully added to database')
     except Exception as exc:
@@ -33,7 +43,11 @@ def add_data():
 def remove_data():
     """Remove all data from the database"""
     try:
-        db.session.execute('TRUNCATE TABLE authors') # usuniecie wszystkiego z tabeli oraz resetacja klucza glownego
+        # db.session.execute('TRUNCATE TABLE authors') # usuniecie wszystkiego z tabeli oraz resetacja klucza glownego, to nie wykona sie popranie jesli mamy klucz obcy
+        db.session.execute('DELETE TABLE books')
+        db.session.execute('ALTER TABLE books AUTO_INCREMENT = 1') # restacja klucz glownego
+        db.session.execute('DELETE TABLE authors')
+        db.session.execute('ALTER TABLE authors AUTO_INCREMENT = 1') # restacja klucz glownego
         db.session.commit()
         print('Data has been successfully removed to database')
     except Exception as exc:
