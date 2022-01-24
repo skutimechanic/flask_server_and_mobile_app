27efe63977bdc928fbc4fdc8d2c1a9719b67320b
Jakub <koszowskimtb@gmail.com>
2022-01-24 22:20:10 +0100

    added missing blueprint

--- server/movie_library_app/__init__.py
+++ server/movie_library_app/__init__.py
@@ -19,8 +19,10 @@ def create_app(config_class=Config):
     from movie_library_app.commands import db_manage_bp
     from movie_library_app.errors import errors_bp
     from movie_library_app.movies import movies_bp
+    from movie_library_app.auth import auth_bp
     app.register_blueprint(db_manage_bp)
     app.register_blueprint(errors_bp)
     app.register_blueprint(movies_bp, url_prefix='/api/v1')
+    app.register_blueprint(auth_bp, url_prefix='/api/v1/auth')
 
     return app
