91ad24ceebae658fb12cdbd6e9e440f811ef6eb1
Lukasz Kwiotek <lukasz.kwiotek@gmail.com>
2022-01-26 16:23:04 +0100

    Update all movies list view

--- client/app/src/main/java/com/polsl/movielibrary/utils/BindingUtils.kt
+++ client/app/src/main/java/com/polsl/movielibrary/utils/BindingUtils.kt
@@ -1,6 +1,7 @@
 package com.polsl.movielibrary.utils
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("app:setImageBackground")
@@ -9,3 +10,15 @@ fun setImageBackground(view: ImageView, url: String?) {
         view.setImageFromUrl(url)
     }
 }
+
+@BindingAdapter("app:setMovieRating")
+fun setMovieRating(view: TextView, ratingSum: Int) {
+    val rating: Float = ratingSum / 10.0f
+    view.text = rating.toString()
+}
+
+@BindingAdapter("app:setNumberString")
+fun setNumberString(view: TextView, year: Any) {
+    view.text = year.toString()
+}
+
