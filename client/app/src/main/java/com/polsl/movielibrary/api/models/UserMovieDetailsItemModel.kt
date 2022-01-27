63ded279ae522e69791e318934006801fd767b23
Lukasz Kwiotek <lukasz.kwiotek@gmail.com>
2022-01-27 17:58:02 +0100

    Movie details screen

--- client/app/src/main/java/com/polsl/movielibrary/api/models/UserMovieDetailsItemModel.kt
+++ client/app/src/main/java/com/polsl/movielibrary/api/models/UserMovieDetailsItemModel.kt
@@ -4,5 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class UserMovieDetailsItemModel(
         @SerializedName("movie") val movie: MovieDetailsItemModel,
-        @SerializedName("rate") val rate: Int,
+        @SerializedName("rate") val rate: Int?,
 )
--- client/app/src/main/java/com/polsl/movielibrary/utils/BindingUtils.kt
+++ client/app/src/main/java/com/polsl/movielibrary/utils/BindingUtils.kt
@@ -3,6 +3,7 @@ package com.polsl.movielibrary.utils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.google.android.material.slider.Slider
 
 @BindingAdapter("app:setImageBackground")
 fun setImageBackground(view: ImageView, url: String?) {
@@ -21,8 +22,34 @@ fun setMovieRating(view: TextView, ratingSum: Int?) {
     }
 }
 
+@BindingAdapter("app:setMovieRatingWithRange")
+fun setMovieRatingWithRange(view: TextView, ratingSum: Int?) {
+    if (ratingSum == null) {
+        view.text = "--"
+    } else {
+        val rating: Float = ratingSum / 10.0f
+        view.text = "$rating/10.0"
+    }
+}
+
 @BindingAdapter("app:setNumberString")
 fun setNumberString(view: TextView, year: Any) {
     view.text = year.toString()
+}
+
+@BindingAdapter("app:setMovieRatingSlider")
+fun setMovieRatingSlider(view: Slider, ratingSum: Int?) {
+    when {
+        ratingSum == null || ratingSum < 0.0f -> {
+            view.value = 0.0f
+        }
+        ratingSum > 100.0f -> {
+            view.value = 10.0f
+        }
+        else -> {
+            val rating: Float = ratingSum / 10.0f
+            view.value = rating
+        }
+    }
 }
 
